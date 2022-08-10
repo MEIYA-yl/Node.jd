582a042cda137ac5db6699c69e980e6070222594
MEIYA-yl <2648297163@qq.com>
2022-08-10 21:22:46 +0800

    express 请求使用

--- express/index.js
+++ express/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const route = require("./router/routingMiddleware");
 
+// 配置 post 请求参数规则
+app.use(express.urlencoded({ extended: false })); // 适用于字符串拼接的参数传递
+app.use(express.json()); // 适用于json格式的请求
+
 app.use(function ficker(req, res, next) {
   console.log("测试 应用级 中间件的使用 ~");
   next();
@@ -10,12 +14,16 @@ app.use(function ficker(req, res, next) {
 
 app.use("/api", route); // 服务于路由中间件
 
-app
-  .get("/aa*bb", (req, res) => {
-    // send 代替了write和end在原生种的命令
-    res.send({
-      name: "zhangsan",
-      age: 23,
-    });
-  })
-  .listen(3000);
+app.get("/aa*bb", (req, res) => {
+  // send 代替了write和end在原生种的命令
+  res.send({
+    name: "zhangsan",
+    age: 23,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send("error");
+});
+
+app.listen(3000);
--- express/router/routingMiddleware.js
+++ express/router/routingMiddleware.js
@@ -3,11 +3,16 @@ const route = express.Router();
 
 // 路由级中间件
 route.get("/home", (req, res) => {
+  console.log("get", req.query);
   res.send("home");
 });
 
-route.get("/login", (req, res) => {
-  res.send("login");
-});
+route.post("/login", (req, res) => {
+  console.log("post", req.body);
+  res.send({
+    code: "200",
+    massage: "post request",
+  });
+}); // post 请求的响应依赖于中间件
 
 module.exports = route;
