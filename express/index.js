e9e4e366a664273206bc701a6cf6aeb75c78b56b
MEIYA-yl <2648297163@qq.com>
2022-08-11 18:23:24 +0800

    静态资源的使用配置

--- express/index.js
+++ express/index.js
@@ -12,7 +12,7 @@ app.use(function ficker(req, res, next) {
 }); // 会致使 use 之后的请求去执行中间件
 // app.use('/aabb', ficker) // 只在该路径下时，中间件才会被触发
 
-app.use("/api", route); // 服务于路由中间件
+app.use("/login", route); // 服务于路由中间件
 
 app.get("/aa*bb", (req, res) => {
   // send 代替了write和end在原生种的命令
@@ -21,6 +21,10 @@ app.get("/aa*bb", (req, res) => {
     age: 23,
   });
 });
+
+// 配置静态资源的访问
+app.use(express.static("public")); // 会直接访问到文件夹下的资源 访问地址不需要田间静态资源文件夹路径
+// app.use("/public", express.static("public")); 需要在访问时添加 路径
 
 app.use((req, res) => {
   res.status(404).send("error");
--- express/router/routingMiddleware.js
+++ express/router/routingMiddleware.js
@@ -2,17 +2,24 @@ const express = require("express");
 const route = express.Router();
 
 // 路由级中间件
-route.get("/home", (req, res) => {
+route.get("/", (req, res) => {
   console.log("get", req.query);
-  res.send("home");
+  res.send("路由中间件：get请求触发~");
 });
 
-route.post("/login", (req, res) => {
+route.post("/", (req, res) => {
   console.log("post", req.body);
-  res.send({
-    code: "200",
-    massage: "post request",
-  });
+  const { username, password } = req.body;
+  if (username == "zhangsan" && password == "asdasd") {
+    res.send({
+      code: "200",
+      massage: "post request",
+    });
+  } else {
+    res.send({
+      code: "500",
+    });
+  }
 }); // post 请求的响应依赖于中间件
 
 module.exports = route;
