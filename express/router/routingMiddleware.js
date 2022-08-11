af90092cbd9b29e711d0e3b124da0e4b3ddecef9
MEIYA-yl <2648297163@qq.com>
2022-08-11 21:31:10 +0800

    了解服务端的渲染

--- express/router/routingMiddleware.js
+++ express/router/routingMiddleware.js
@@ -7,17 +7,21 @@ route.get("/", (req, res) => {
   res.send("路由中间件：get请求触发~");
 });
 
+route.get("/list", (req, res) => {
+  res.send([1, 2, 3, 4, 5]);
+});
+
 route.post("/", (req, res) => {
   console.log("post", req.body);
   const { username, password } = req.body;
-  if (username == "zhangsan" && password == "asdasd") {
+  if (username == "zhangsan" && password == "121212") {
     res.send({
-      code: "200",
+      code: 200,
       massage: "post request",
     });
   } else {
     res.send({
-      code: "500",
+      code: 500,
     });
   }
 }); // post 请求的响应依赖于中间件
