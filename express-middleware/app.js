e6bf319224d71aa8f2bd5929c26d7a2183869e4c
MEIYA-yl <2648297163@qq.com>
2022-08-22 22:03:50 +0800

    错误中间件，处理404

--- express-middleware/app.js
+++ express-middleware/app.js
@@ -8,7 +8,8 @@ app.use((req, res, next) => {
 // 中间件在数组中声明为可重用
 const logOrginalUrl = (req, res, next) => {
   console.log("Request URL: ", req.originalUrl);
-  next();
+  if (req.params.id === "0") next("route"); // next('route')
+  else next();
 };
 
 const logMethod = (req, res, next) => {
@@ -21,21 +22,23 @@ app.get("/user/:id", logStuff, (req, res, next) => {
   res.send("user info");
 });
 
-// next('route')
-app.get(
-  "/user/:id",
-  function (req, res, next) {
-    if (req.params.id === "0") next("route");
-    else next();
-  },
-  function (req, res, next) {
-    res.send("regular");
-  }
-);
-
 app.get("/user/:id", function (req, res, next) {
-  res.send("special");
+  try {
+    res.send("special");
+  } catch (err) {
+    next(err); // 将err通过next()传递给 错误中间件
+  }
 });
+
+app.use((req, res, next) => {
+  res.status(404).end("404 Not Found !");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message,
+  });
+}); // 错误中间件
 
 app.listen(3000, () => {
   console.log("server runing at http://localhost:3000");
