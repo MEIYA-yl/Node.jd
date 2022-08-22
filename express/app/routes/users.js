692e09e97b4de1333e10bff319d52247aeea8ca0
MEIYA-yl <2648297163@qq.com>
2022-08-22 11:03:31 +0800

    express patch: grammar

--- express/app/routes/users.js
+++ express/app/routes/users.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.cookie("city", "beijing"); // res.cookie 设置cookie， req.cookie 读取
-  res.send("respond with a resource");
+  // res.statusCode = 201 node 原生的方式设置响应状态码
+  res.status(201).send("respond with a resource");
 });
 
 module.exports = router;
