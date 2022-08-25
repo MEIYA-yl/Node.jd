541d4698ed598ac099eeb068b3ab2942783236df
MEIYA-yl <2648297163@qq.com>
2022-08-25 09:58:25 +0800

    配置错误处理中间件

--- express-api-exercise/app.js
+++ express-api-exercise/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan"); // 日志输出
 const cors = require("cors"); // 为客户端提供跨域资源请求
 
 const router = require("./router");
+const errorHandler = require("./middleware/error-handler");
 const PORT = process.env.PORT || 3300;
 
 app.use(morgan("dev"));
@@ -14,13 +15,11 @@ app.use(express.urlencoded());
 
 // 挂载路由中间件
 app.use("/api", router);
-
+// 处理 404
 app.use((req, res, next) => {
   res.status(404).end("404 Not Found!");
 });
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    error: err.message,
-  });
-});
+// 挂载错误中间件
+app.use(errorHandler());
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
